2babf6a71c236f4fbf43300dc5de8c71e461129a
jasmine-5p2 <jasminekollapu@gmail.com>
2024-11-23 15:55:33 +0530

    first commit

---
+++ Prime.java
@@ -0,0 +1,30 @@
+class Prime{
+    public static void main(String args[])
+    {
+        int n=6;
+        if(isPrime(n))
+        {
+            System.out.println("prime");
+        }
+        else
+        {
+            System.out.println("not prime");
+        }
+    }
+    public static boolean isPrime(int n)
+    {
+        if(n<1)
+         {
+            return false;
+         }
+         for(int i=2;i<=Math.sqrt(n);i++)
+         {
+            if(n%i==0)
+            {
+                return false;
+            }
+         }
+         return true;
+    
+    }
+}
